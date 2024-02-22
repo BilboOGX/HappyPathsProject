70560016390fb3272313bce25084f0f67fc22a44
S Rankin <147388174+BilboOGX@users.noreply.github.com>
2024-02-22 14:03:27 +0000

    File Merge

--- app/api/books.ts
+++
@@ -1,5 +0,0 @@
-export const getBookByISBN = async (isbn: string) => {
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
-    return await response.json();
-  };
-  
