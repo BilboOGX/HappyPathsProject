99a79491c78969b329b9817a3e59b893eb1a8e40
kneeshuh <ni.gettings@gmail.com>
2024-02-24 19:16:25 +0000

    progress on working out how to fetch and assign correct user data to profile

--- app/screens/Profile.tsx
+++ app/screens/Profile.tsx
@@ -1,17 +1,75 @@
-import { View, Text, Button, Image, TouchableOpacity, StyleSheet } from 'react-native'
-import React, { useState, useEffect } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { MaterialIcons } from '@expo/vector-icons'
-import { FIREBASE_AUTH } from '../../FireBaseConfig'
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialIcons } from "@expo/vector-icons";
+import { FIREBASE_AUTH, FIREBASE_DB } from "../../FireBaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 
-const Profile = ({navigation}: any) => {
+const Profile = ({ navigation }: any) => {
+  const [users, setUsers] = useState([]);
+  const [currUser, setCurrUser] = useState(FIREBASE_AUTH.currentUser);
+  const fetchUserFromFirestore = async () => {
+    try {
+      const collectionRef = collection(FIREBASE_DB, "users");
+      const snapshot = await getDocs(collectionRef);
+      const fetchedUsers = [];
+      snapshot.forEach((user) => {
+        console.log(user);
+        fetchedUsers.push({
+          id: user.id,
+          ...user.data(),
+        });
+      });
+      setUsers(fetchedUsers);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // console.log(users, '<-- users in profile')
+  // console.log(FIREBASE_AUTH.currentUser, '<-- current user')
+  useEffect(() => {
+    fetchUserFromFirestore();
+  }, []); // need something to refresh when user is updated, currUser causes infinite loop
+  // const assignCurrUser = () => {
+  //   for (let i = 0; i < users.length; i++) {
+  //     if (users[i].userUID === FIREBASE_AUTH.currentUser?.uid) {
+  //       setCurrUser(users[i]);
+  //     }
+  //   }
+  // };
+  // console.log(currUser, "<-- current user?");
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.avatarContainer}>
-        <Image source={{
-          uri: FIREBASE_AUTH.currentUser?.photoURL
-        }} style={styles.avatar}/>
-      <Text style={styles.name}>{FIREBASE_AUTH.currentUser?.displayName}</Text>
+        {users.map((user) => {
+          console.log(user, "<-- user");
+          if (user.id === currUser.uid) {
+            return (
+              // if user has avatarURL return that, else return photoURL?
+              <Image
+                source={{ uri: currUser.photoURL }}
+                style={styles.avatar}
+              />
+            );
+          }
+        })}
+        <Image
+          source={{
+            uri: currUser.photoURL,
+          }}
+          style={styles.avatar}
+        />
+        <Text style={styles.name}>
+          {FIREBASE_AUTH.currentUser?.displayName}
+        </Text>
       </View>
       <View style={styles.infoContainer}>
         <Text style={styles.infoLabel}>Email: </Text>
@@ -21,15 +79,24 @@ const Profile = ({navigation}: any) => {
         <Text style={styles.infoLabel}>User ID: </Text>
         <Text style={styles.infoValue}>{FIREBASE_AUTH.currentUser?.uid}</Text>
       </View>
-    <View style={styles.buttonContainer}>
-      <Button onPress={() => navigation.navigate('EditProfile')} title='Edit Profile' />
-      <Button onPress={() => navigation.navigate('MyListings')} title='Go to my listings'/>
-      <Button onPress={() => navigation.navigate('MyFavourites')} title='Go to my favourites' />
-      <Button onPress={() => FIREBASE_AUTH.signOut()} title="Logout" />
-    </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={() => navigation.navigate("EditProfile")}
+          title="Edit Profile"
+        />
+        <Button
+          onPress={() => navigation.navigate("MyListings")}
+          title="Go to my listings"
+        />
+        <Button
+          onPress={() => navigation.navigate("MyFavourites")}
+          title="Go to my favourites"
+        />
+        <Button onPress={() => FIREBASE_AUTH.signOut()} title="Logout" />
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -39,7 +106,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     alignItems: "center",
-    marginTop: 50
+    marginTop: 50,
   },
   avatarContainer: {
     alignItems: "center",
@@ -68,4 +135,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Profile
+export default Profile;
