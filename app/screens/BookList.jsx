d52bea89e0e20680a5198fab288d7834f4e2aa99
Henry Westhoff-Lewis <henry@192.168.1.100>
2024-02-24 14:16:43 +0000

    BookList filtering from searchBar

--- app/screens/BookList.jsx
+++ app/screens/BookList.jsx
@@ -1,32 +1,25 @@
-import { View, Text, StyleSheet, Button, FlatList, ImageBackground, Pressable} from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { FIREBASE_DB } from '../../FireBaseConfig';
-import { addDoc, collection, getDocs } from 'firebase/firestore';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  ImageBackground,
+  Pressable,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { FIREBASE_DB } from "../../FireBaseConfig";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
-// const BookList = () => {
-//   const test = async () => {
-//     console.log('add')
-
-//     const doc = addDoc(collection(FIREBASE_DB, 'todos'), {title: 'I am a test', done: false})
-//     console.log(doc, 'done')
-
-//   }
-
-//   return (
-//     <View>
-//       <Button onPress={test} title='test add'/>
-//     </View>
-//   )
-// }
-
-// export default BookList;
-
-const BookList = ({navigation}) => {
+const BookList = ({ navigation }) => {
   const [data, setData] = useState([]);
+  const [fullData, setFullData] = useState([])
+  const [searchQuery, setSearchQuery] = useState('')
 
   const fetchDataFromFirestore = async () => {
     try {
-      const collectionRef = collection(FIREBASE_DB, 'books');
+      const collectionRef = collection(FIREBASE_DB, "books");
       const snapshot = await getDocs(collectionRef);
       // console.log(snapshot)
       const fetchedData = [];
@@ -36,12 +29,10 @@ const BookList = ({navigation}) => {
           ...doc.data(),
         });
       });
-
-      // console.log(fetchedData);
-      // console.log(fetchedData[0].coords.latitude);
       setData(fetchedData);
+      setFullData(fetchedData)
     } catch (error) {
-      console.error('Error fetching data: ', error);
+      console.error("Error fetching data: ", error);
     }
   };
 
@@ -49,56 +40,93 @@ const BookList = ({navigation}) => {
     fetchDataFromFirestore();
   }, []); // useEffect to fetch data when the component mounts
 
+  const handleSearch = (query) => {
+    if (query) {
+
+      console.log(query)
+      console.log(data.bookTitle, 'data')
+      setSearchQuery(query)
+      const formattedQuery = query.toLowerCase()
+  
+      let filteredData = fullData.filter((book) => {
+        return book.bookTitle.toLowerCase().includes(formattedQuery)
+      })
+      console.log(filteredData)
+  
+      setData(filteredData)
+    }
+    else {
+      setData(fullData)
+    }
+  }
+
   return (
-    <ImageBackground 
-    source={require('../../Images/wp13203104.jpg')}
-    style={{flex: 1}} 
-  >
-    <View style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={({item}) => (
-          <Pressable onPress={() => navigation.navigate('SingleBookPage', {
-            title: item.bookTitle
-          })}>
-
-          <ImageBackground source={require('../../Images/blank_vintage_book_by_vixen525_d600pp8-fullview.png')}
-            style={styles.bookContainer}>
-            <Text style={styles.text}>{item.bookTitle}</Text>
-            <Text>{item.bookAuthor}</Text>
-            <Text>{item.bookDesc}</Text>
-            <Text>{item.uploadedBy}</Text>
-          
-          </ImageBackground>
-
-          </Pressable>
-        )}
-        keyExtractor={item => item.id}
-      />
-    </View>
+    <ImageBackground
+      source={require("../../Images/wp13203104.jpg")}
+      style={{ flex: 1 }}
+    >
+      <View style={styles.container}>
+        <TextInput
+          style={{
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            margin: 10,
+            borderColor: "#ccc",
+            borderWidth: 1,
+            borderRadius: 8,
+            backgroundColor: 'white',
+            width: 300
+          }}
+          placeholder="Search"
+          clearButtonMode="always"
+          autoCapitalize="none"
+          autoCorrect={false}
+          onChangeText={((query) => handleSearch(query))}
+        />
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <Pressable
+              onPress={() =>
+                navigation.navigate("SingleBookPage", {
+                  title: item.bookTitle,
+                })
+              }
+            >
+              <ImageBackground
+                source={require("../../Images/blank_vintage_book_by_vixen525_d600pp8-fullview.png")}
+                style={styles.bookContainer}
+              >
+                <Text style={styles.text}>{item.bookTitle}</Text>
+                <Text>{item.bookAuthor}</Text>
+                <Text>{item.bookDesc}</Text>
+                <Text>{item.uploadedBy}</Text>
+              </ImageBackground>
+            </Pressable>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
   },
 
   bookContainer: {
-
     margin: 5,
     width: 200,
     height: 200,
-    
-  
   },
   text: {
     margin: 5,
-  }
+  },
 });
 
 export default BookList;
