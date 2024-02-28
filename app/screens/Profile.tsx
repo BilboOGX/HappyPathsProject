998429c75875da51f2ca1d73936e73a42baa02f8
S Rankin <147388174+BilboOGX@users.noreply.github.com>
2024-02-28 15:12:08 +0000

    uniform style

--- app/screens/Profile.tsx
+++ app/screens/Profile.tsx
@@ -59,66 +59,80 @@ const Profile = ({ navigation, route }: any) => {
 
   // if route.params.updatedUser !== null/undefined, render those details, otherwise render these current ones
   // if (route.params !== undefined) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.avatarContainer}>
-        <Image
-          source={{ uri: currUser.photoURL }}
-          style={styles.avatar}
-        />
-        <Text style={styles.name}>{currUser.username}</Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.infoLabel}>Email: </Text>
-        <Text style={styles.infoValue}>{currUser.email}</Text> 
-        {/* why does it immediately get the current user's email but not immediately get other properties? */}
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.infoLabel}>Location: </Text>
-        <Text style={styles.infoValue}>{currUser.location}</Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.infoLabel}>User ID:</Text>
-        <Text style={styles.infoValue}>{currUser.uid}</Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          onPress={() => navigation.navigate("EditProfile", {
-            user: currUser,
-            // updatedUser: updatedUser
-          })}
-          title="Edit Profile"
-        />
-        <Button
-          onPress={() => navigation.navigate("MyListings", {
-            user: currUser
-          })}
-          title="Go to my listings"
-        />
-        <Button
-          onPress={() => navigation.navigate("MyFavourites", { uid: currUser.userUID })}
-          title="Go to my favourites"
-        />
-        <Button onPress={() => FIREBASE_AUTH.signOut()} title="Logout" />
-      </View>
-    </SafeAreaView>
-  );
-};
-//}
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.profile}>
+          <View style={styles.avatarContainer}>
+            <Image source={{ uri: currUser.photoURL }} style={styles.avatar} />
+            <Text style={styles.name}>{currUser.username}</Text>
+          </View>
+    
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoLabel}>Email: </Text>
+            <Text style={styles.infoValue}>{currUser.email}</Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoLabel}>Location: </Text>
+            <Text style={styles.infoValue}>{currUser.location}</Text>
+          </View>
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoLabel}>User ID:</Text>
+            <Text style={styles.infoValue}>{currUser.uid}</Text>
+          </View>
+          <View style={styles.maninButtonContainer}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.buttonTwo} onPress={() => navigation.navigate("EditProfile", {
+                user: currUser,
+              })}>
+              <Text style={styles.buttonText}>Edit Profile</Text>
+            </TouchableOpacity>
+    
+            <TouchableOpacity style={styles.buttonTwo} onPress={() => navigation.navigate("MyListings", {
+                user: currUser,
+              })}>
+              <Text style={styles.buttonText}>My Listings</Text>
+            </TouchableOpacity>
+    
+            <TouchableOpacity style={styles.buttonTwo} onPress={() => navigation.navigate("MyFavourites", { uid: currUser.userUID })}>
+              <Text style={styles.buttonText}>My Favourites</Text>
+            </TouchableOpacity>
+    
+            <TouchableOpacity style={styles.buttonTwo} onPress={() => FIREBASE_AUTH.signOut()}>
+              <Text style={styles.buttonText}>Logout</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </View>
+      </SafeAreaView>
+    );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    padding: 20,
+    backgroundColor: '#00592e',
+    padding: 0,
   },
-  buttonContainer: {
-    alignItems: "center",
-    marginTop: 50,
+  profile: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: '95%',
+    height: '100%',
+    flexGrow: 0,
+    padding: 0,
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 20,
+    
   },
+  // buttonContainer: {
+  //   alignItems: "center",
+  //   marginTop: 0,
+  // },
   avatarContainer: {
     alignItems: "center",
-    marginTop: 20,
+    marginTop: 0,
   },
   avatar: {
     width: 150,
@@ -130,10 +144,13 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 20,
     fontWeight: "bold",
-    marginTop: 10,
+    marginTop: 3,
+    marginBottom: 10,
   },
   infoContainer: {
-    marginTop: 20,
+    marginTop: 5,
+    marginLeft: 20,
+    marginBottom: 5,
   },
   infoLabel: {
     fontWeight: "bold",
@@ -141,6 +158,29 @@ const styles = StyleSheet.create({
   infoValue: {
     marginTop: 5,
   },
+  buttonContainer: {
+    flexDirection: "column",
+    width: "50%",
+    marginTop: 15,
+  },
+  buttonTwo: {
+    backgroundColor: "#00592e",
+    padding: 10,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    margin:1,
+
+  },
+  maninButtonContainer: {
+    alignItems: 'center',
+    margin:5,
+    marginTop: 10,
+   },
+  buttonText:{
+    color: 'white',
+  }
 });
 
 export default Profile;
